b91cce387056d7b07e8c735b473f79be43c2669b
Alex Garbarino <alexiothebrave@gmail.com>
2024-12-09 21:44:22 -0600

    almost done with Phase 0!

--- backend/app.js
+++ backend/app.js
@@ -6,10 +6,10 @@ const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const routes = require('./routes');
-app.use(routes); // Connect all the routes
 const { environment } = require('./config');
 const isProduction = environment === 'production';
 const app = express();
+app.use(routes); // Connect all the routes
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
