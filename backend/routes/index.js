c862b6f1b11817bd3506b26e85608f72c5f820b9
danehoov <DaneHoov22@gmail.com>
2024-12-10 20:29:12 -0700

    Remove test route, add api csrf restore

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,11 +4,16 @@ const apiRouter = require("./api");
 const router = express.Router();
 router.use("/api", apiRouter);
 
-router.get("/hello/world", function (req, res) {
-  console.log(req.csrfToken)
-  res.cookie("XSRF-TOKEN", req.csrfToken());
-  res.send("Hello World!");
-  // res.json({'XSRF-Token': req.csrfToken()})
+// backend/routes/index.js
+// ...
+// Add a XSRF-TOKEN cookie
+router.get("/api/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    "XSRF-Token": csrfToken,
+  });
 });
+// ...
 
 module.exports = router;
