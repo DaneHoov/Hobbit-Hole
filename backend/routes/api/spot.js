f221cf60ccb5c3624539b9f77cc427608c6c53f4
Alex Garbarino <alexiothebrave@gmail.com>
2024-12-17 21:06:43 -0600

    done for the night

--- backend/routes/api/spot.js
+++ backend/routes/api/spot.js
@@ -48,6 +48,19 @@ router.post('/spots', validateSpot, async (req, res) => {
         city: spot.city,
         state: spot.state,
         country: spot.country,
-        
+        lat: spot.lat,
+        lng: spot.lng,
+        name: spot.name,
+        description: spot.description,
+        price: spot.price
     }
+
+    await setTokenCookie(res, safeUser);
+
+    return res.json({
+        spot: safeSpot
+      });
 })
+
+
+module.exports = router
