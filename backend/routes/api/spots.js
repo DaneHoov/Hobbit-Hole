36f09d3eab070f3392e8cb9cb28e62e330f99dfd
danehoov <DaneHoov22@gmail.com>
2024-12-30 19:09:56 -0700

    all spots by current user

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -11,68 +11,99 @@ const validateSpot = [
   check("address")
     .exists({ checkFalsy: true })
     .withMessage("Street address is required"),
-  check("city")
-    .exists({ checkFalsy: true })
-    .withMessage("City is required"),
-  check("state")
-    .exists({ checkFalsy: true })
-    .withMessage("State is required"),
+  check("city").exists({ checkFalsy: true }).withMessage("City is required"),
+  check("state").exists({ checkFalsy: true }).withMessage("State is required"),
   check("country")
     .exists({ checkFalsy: true })
     .withMessage("Country is required"),
   check("lat")
-    .exists({ checkFalsy: true})
-    .isFloat({min: -90, max: 90})
+    .exists({ checkFalsy: true })
+    .isFloat({ min: -90, max: 90 })
     .withMessage("Latitude must be within -90 and 90"),
   check("lng")
-    .exists({checkFalsy: true})
-    .isFloat({min: -180, max: 180})
+    .exists({ checkFalsy: true })
+    .isFloat({ min: -180, max: 180 })
     .withMessage("Longitude must be within -180 and 180"),
   check("name")
-    .exists({checkFalsy: true})
-    .isLength({min: 1, max: 49})
+    .exists({ checkFalsy: true })
+    .isLength({ min: 1, max: 49 })
     .withMessage("Name must be less than 50 characters"),
   check("description")
     .exists({ checkFalsy: true })
     .withMessage("Description is required"),
   check("price")
-    .isInt({min: 1})
+    .isInt({ min: 1 })
     .withMessage("Price per day must be a positive number"),
   handleValidationErrors,
-]
+];
 
 //Create a spot
-router.post('/', requireAuth, validateSpot, async (req, res) => {
-  const {address, city, state, country, lat, lng, name, description, price} = req.body;
-  const spot = await Spot.create({ownerId: req.user.id, address, city, state, country, lat, lng, name, description, price});
+router.post("/", requireAuth, validateSpot, async (req, res) => {
+  const { address, city, state, country, lat, lng, name, description, price } =
+    req.body;
+  const spot = await Spot.create({
+    ownerId: req.user.id,
+    address,
+    city,
+    state,
+    country,
+    lat,
+    lng,
+    name,
+    description,
+    price,
+  });
 
   const safeSpot = {
-      id: spot.id,
-      ownerId: spot.ownerId,
-      address: spot.address,
-      city: spot.city,
-      state: spot.state,
-      country: spot.country,
-      lat: spot.lat,
-      lng: spot.lng,
-      name: spot.name,
-      description: spot.description,
-      price: spot.price
-  }
-
-
+    id: spot.id,
+    ownerId: spot.ownerId,
+    address: spot.address,
+    city: spot.city,
+    state: spot.state,
+    country: spot.country,
+    lat: spot.lat,
+    lng: spot.lng,
+    name: spot.name,
+    description: spot.description,
+    price: spot.price,
+  };
 
   return res.json({
-      spot: safeSpot
-    });
-})
+    spot: safeSpot,
+  });
+});
 
 //Get all spots
-router.get('/', async(req, res) => {
-  const spots = await Spot.findAll()
+router.get("/", async (req, res) => {
+  const spots = await Spot.findAll();
   res.json(spots);
-})
+});
 
+//Get all spots by current user
+router.get("/", requireAuth, async (req, res) => {
+  const userId = req.user.id;
 
+  const spots = await Spot.findAll({
+    where: { ownerId: userId },
+    attributes: [
+      "id",
+      "ownerId",
+      "address",
+      "city",
+      "state",
+      "country",
+      "lat",
+      "lng",
+      "name",
+      "description",
+      "price",
+      "createdAt",
+      "updatedAt",
+      "avgRating",
+      "previewImage",
+    ],
+  });
+  return res.status(200).json({ Spots: spots });
+});
 
-module.exports = router
+module.exports = router;
