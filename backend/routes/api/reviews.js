2d8d336399deb026c36d8aa07de0a052cc23df2a
danehoov <DaneHoov22@gmail.com>
2025-01-01 14:01:07 -0700

    Add image to a review based on review id

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -2,17 +2,51 @@ const express = require("express");
 const { Op } = require("sequelize");
 const bcrypt = require("bcryptjs");
 
-const { setTokenCookie, restoreUser, requireAuth } = require("../../utils/auth");
+const {
+  setTokenCookie,
+  restoreUser,
+  requireAuth,
+} = require("../../utils/auth");
 const { User, Spot } = require("../../db/models");
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
 const router = express.Router();
 
 const validateReview = [
-    check("review")
-    .exists({checkFalsy: true})
+  check("review")
+    .exists({ checkFalsy: true })
     .withMessage("Review text is required"),
-    check("stars")
-    .isInt({min: 1, max: 5})
-    .withMessage("Stars must be an integer from 1 to 5")
-]
+  check("stars")
+    .isInt({ min: 1, max: 5 })
+    .withMessage("Stars must be an integer from 1 to 5"),
+];
+
+//Add image to review based on review id
+router.post("/:id/images", requireAuth, async (req, res) => {
+  const reviewId = req.params.id;
+  const { url } = req.body;
+  const userId = req.user.id;
+
+  const review = await Review.findByPk(reviewId);
+  if (!review) {
+    return res.status(404).json({ message: "Review couldn't be found" });
+  }
+  if (review.userId !== userId) {
+    return res.status(403).json({ message: "Unauthorized" });
+  }
+
+  //review already has 10 images
+  const imageCount = await ReviewImage.count({ where: { reviewId } });
+  if (imageCount >= 10) {
+    return res.status(403).json({
+      message: "Maximum number of images for this resource was reached",
+    });
+  }
+
+  const newImage = await ReviewImage.create({
+    reviewId,
+    url,
+  });
+
+  return res.status(201).json(newImage);
+});
