20b500dc6c3f7382479c91069a92f2899ee15fed
danehoov <DaneHoov22@gmail.com>
2025-01-01 14:33:34 -0700

    Delete review image

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -121,3 +121,47 @@ router.delete(
     });
   }
 );
+
+//Delete review image
+router.delete(
+  "/api/session/reviews/:reviewId/images/:imageId",
+  authenticate,
+  async (req, res) => {
+    const userId = req.user.id;
+    const reviewId = req.params.reviewId;
+    const imageId = req.params.imageId;
+
+    const review = await Review.findByPk(reviewId);
+
+    if (!review) {
+      return res.status(404).json({
+        message: "Review couldn't be found",
+      });
+    }
+
+    if (review.userId !== userId) {
+      return res.status(403).json({
+        message: "You don't have permission to delete this image",
+      });
+    }
+
+    const reviewImage = await ReviewImage.findOne({
+      where: {
+        id: imageId,
+        reviewId: reviewId,
+      },
+    });
+
+    if (!reviewImage) {
+      return res.status(404).json({
+        message: "Review Image couldn't be found",
+      });
+    }
+
+    await reviewImage.destroy();
+
+    res.status(200).json({
+      message: "Successfully deleted",
+    });
+  }
+);
