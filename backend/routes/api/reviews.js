ce92ee1527180033f24c5f73c399eed73948dcfa
Agarbo1 <161474119+Agarbo1@users.noreply.github.com>
2025-01-06 20:48:49 -0600

    Update reviews.js

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -7,7 +7,7 @@ const {
   restoreUser,
   requireAuth,
 } = require("../../utils/auth");
-const { User, Spot } = require("../../db/models");
+const { Review } = require("../../db/models");
 const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
 const router = express.Router();
@@ -21,38 +21,49 @@ const validateReview = [
     .withMessage("Stars must be an integer from 1 to 5"),
 ];
 
-//Add image to review based on review id
-router.post("/:id/images", requireAuth, async (req, res) => {
-  const reviewId = req.params.id;
-  const { url } = req.body;
+//get all reviews of current user
+router.get("/current", requireAuth, async (req, res) => {
   const userId = req.user.id;
 
-  const review = await Review.findByPk(reviewId);
-  if (!review) {
-    return res.status(404).json({ message: "Review couldn't be found" });
-  }
-  if (review.userId !== userId) {
-    return res.status(403).json({ message: "Unauthorized" });
-  }
-
-  //review already has 10 images
-  const imageCount = await ReviewImage.count({ where: { reviewId } });
-  if (imageCount >= 10) {
-    return res.status(403).json({
-      message: "Maximum number of images for this resource was reached",
-    });
-  }
-
-  const newImage = await ReviewImage.create({
-    reviewId,
-    url,
+  const reviews = await Review.findAll({
+    where: { userId },
+    include: [
+      {
+        model: User,
+        attributes: ["id", "firstName", "lastName"],
+      },
+      {
+        model: Spot,
+        attributes: [
+          "id",
+          "ownerId",
+          "address",
+          "city",
+          "state",
+          "country",
+          "lat",
+          "lng",
+          "name",
+          "price",
+          "previewImage"
+        ],
+      },
+      {
+        model: ReviewImage,
+        attributes: [
+          "id",
+          "url",
+        ],
+      },
+    ],
   });
 
-  return res.status(201).json(newImage);
+  return res.status(200).json({ Reviews: reviews });
 });
 
+
 //Edit review
-router.put("/reviews/:id", requireAuth, async (req, res) => {
+router.put("/:id", requireAuth, async (req, res) => {
   const { review, stars } = req.body;
   const reviewId = req.params.id;
 
@@ -89,7 +100,7 @@ router.put("/reviews/:id", requireAuth, async (req, res) => {
 
   res.status(200).json({
     id: reviewToUpdate.id,
-    userId: reviewToUpdate.userId,
+    ownerId: reviewToUpdate.ownerId,
     spotId: reviewToUpdate.spotId,
     review: reviewToUpdate.review,
     stars: reviewToUpdate.stars,
@@ -97,4 +108,105 @@ router.put("/reviews/:id", requireAuth, async (req, res) => {
     updatedAt: reviewToUpdate.updatedAt,
   });
 });
+
+//Add image to review based on review id
+router.post("/:id/images", requireAuth, async (req, res) => {
+  const reviewId = req.params.id;
+  const { url } = req.body;
+  const userId = req.user.id;
+
+  const review = await Review.findByPk(reviewId);
+  if (!review) {
+    return res.status(404).json({ message: "Review couldn't be found" });
+  }
+  if (review.userId !== userId) {
+    return res.status(403).json({ message: "Unauthorized" });
+  }
+
+  //review already has 10 images
+  const imageCount = await ReviewImage.count({ where: { reviewId } });
+  if (imageCount >= 10) {
+    return res.status(403).json({
+      message: "Maximum number of images for this resource was reached",
+    });
+  }
+
+  const newImage = await ReviewImage.create({
+    reviewId,
+    url,
+  });
+
+  return res.status(201).json(newImage);
+});
+
+//Delete review image
+router.delete(
+  "/:reviewId/images/:imageId",
+  requireAuth,
+  async (req, res) => {
+    const ownerId = req.user.id;
+    const reviewId = req.params.reviewId;
+    const imageId = req.params.imageId;
+
+    const review = await Review.findByPk(reviewId);
+
+    if (!review) {
+      return res.status(404).json({
+        message: "Review couldn't be found",
+      });
+    }
+
+    if (review.userId !== ownerId) {
+      return res.status(403).json({
+        message: "You don't have permission to delete this image",
+      });
+    }
+
+    const reviewImage = await ReviewImage.findOne({
+      where: {
+        id: imageId,
+        reviewId: reviewId,
+      },
+    });
+
+    if (!reviewImage) {
+      return res.status(404).json({
+        message: "Review Image couldn't be found",
+      });
+    }
+
+    await reviewImage.destroy();
+
+    res.status(200).json({
+      message: "Successfully deleted",
+    });
+  }
+);
+
+//Delete a review
+router.delete(
+  "/:id",
+  requireAuth,
+  async (req, res) => {
+    const reviewId = req.params.id;
+
+    const reviewToDelete = await Review.findByPk(reviewId);
+
+
+    if (!reviewToDelete) {
+      return res.status(404).json({
+        message: "Review couldn't be found",
+      });
+    }
+
+    await reviewToDelete.destroy();
+
+
+    res.status(200).json({
+      message: "Successfully deleted",
+    });
+  }
+);
+
+module.exports = router;
 
