1d3d39a719c5f4a53324fa7d89a069399b32c30a
Alex Garbarino <alexiothebrave@gmail.com>
2025-01-07 14:46:52 -0600

    commit

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -19,7 +19,7 @@ module.exports = (sequelize, DataTypes) => {
       }),
       Spot.hasMany(models.SpotImage, {
         foreignKey: 'spotId',
-        // onDelete: "CASCADE"
+        onDelete: "CASCADE"
       }),
       Spot.hasMany(models.Booking, {
         foreignKey: 'spotId'
