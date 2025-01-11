370ed9f7d8d93cdcfe46a63246a42e8b30087613
Alex Garbarino <alexiothebrave@gmail.com>
2025-01-11 14:15:28 -0600

    commit

--- backend/db/seeders/20250101200703-demo-review.js
+++ backend/db/seeders/20250101200703-demo-review.js
@@ -25,7 +25,7 @@ module.exports = {
 
   async down (queryInterface, Sequelize) {
     options.tableName = 'Reviews';
-    const Op = sequelize.Op;
+    const Op = Sequelize.Op;
     return queryInterface.bulkDelete(
       options,
       {
