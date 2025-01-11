7d06022ad72f080d0340c3665f3d8cdf40aa5d2e
Alex Garbarino <alexiothebrave@gmail.com>
2025-01-11 14:19:20 -0600

    commit

--- backend/db/migrations/20241218011607-create-spot.js
+++ backend/db/migrations/20241218011607-create-spot.js
@@ -70,6 +70,7 @@ module.exports = {
   async down(queryInterface, Sequelize) {
     options.tableName = 'Spots'
     // await queryInterface.removeConstraint("hobbit_hole_schema.SpotImages", "SpotImages_spotId_fkey")
+    await queryInterface.removeConstraint("hobbit_hole_schema.Reviews", "Reviews_spotId_fkey")
     await queryInterface.dropTable(options);
   }
 };
